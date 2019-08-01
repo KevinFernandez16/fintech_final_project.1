0b08c6f314f1a2cd0e023ef12a93265b2000347f
EC2 Default User <ec2-user@ip-172-31-27-127.us-east-2.compute.internal>
2019-08-01 19:13:06 +0000

    6th commit

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,25 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect, session, url_for
 from app.models import model, formopener
+from flask_pymongo import PyMongo
+
+#secret key for session
+app.secret_key = "anysecretstring"
+# name of database
+app.config['MONGO_DBNAME'] = 'room_aestheticizer' 
+# URI of database
+app.config['MONGO_URI'] = 'mongodb+srv://crystalyang:crystal123@cluster0-xrzh9.mongodb.net/room_aestheticizer?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    if 'username' not in session:
+        nameDisplay = "New User"
+    else:
+        nameDisplay = session['username']
+    return render_template('index.html', nameDisplay = nameDisplay)
     
 @app.route('/results', methods = ['GET', 'POST'])
 def results():
@@ -32,6 +46,47 @@ def results():
         # ikea = model.IkeaURLs(room_type, furniture, color, URL)
         # print(ikea)
             return render_template('results.html', room_type = room_type, furniture = furniture, color = color, URL = URL, URL1 = URL1, URL2 = URL2, error = error )
+@app.route('/signup', methods=['POST','GET'])
+def signup():
+    if 'username' not in session:
+        session['username'] = 'new user'
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name': request.form['username']})
+        
+        if existing_user is None:
+            users.insert({'name': request.form['username'], 'password': request.form['password']})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        else:
+            return 'That username already exists!  Try logging in.'
+    nameDisplay = session['username']
+    return render_template('signup.html', nameDisplay = nameDisplay)
+    
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if 'username' not in session:
+        return render_template('login.html')
+   
+    elif request.method == 'POST':
+        users = mongo.db.users
+        login_data = dict(request.form)
+        login_user = users.find_one({'name' : login_data['username']})
+        if login_user:
+            if login_data['password'] == login_user['password']:
+                session['username'] = login_data['username']
+                return redirect('/index')
+        return 'Invalid username/password combination'
+        
+    else:
+        nameDisplay = session['username']
+        return render_template('login.html', nameDisplay = nameDisplay)
+            
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
+
         
         
         
